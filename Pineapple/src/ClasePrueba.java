5a2c51bdcca2c8e0139128c2cc8dcc7294d0f30f
JavierZaldivar <javipuerto10@172.19.208.149>
2016-10-25 14:01:41 +0200

    Modificicacion de prueba

--- Pineapple/src/ClasePrueba.java
+++ Pineapple/src/ClasePrueba.java
@@ -2,6 +2,6 @@
 public class ClasePrueba {
 	public static void main(String[] args) {
 		System.out.print("Paco guapo, estás mas bueno que el pan");
-		System.out.print("Hola Pacooooooooooo");
+		System.out.print("Hola Paco");
 	}
 }
