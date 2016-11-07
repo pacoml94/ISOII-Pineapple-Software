e5e59f6f9081db9468448c67bed05d83bc109cab
Francisco <Fco.Murillo@alu.uclm.es>
2016-11-07 16:54:18 +0100

    Inmueble.java y proyecto

---
+++ Inmuebles/src/Presentación/Inmueble.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentación;
+
+/**
+ *
+ * @author user
+ */
+public class Inmueble {
+    
+}
