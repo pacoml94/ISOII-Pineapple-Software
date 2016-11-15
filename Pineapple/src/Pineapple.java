0a3d24bcc0afd058f8589d07bba115b9d26fa1e8
Francisco <Fco.Murillo@alu.uclm.es>
2016-11-15 13:26:47 +0100

    prueba

--- Pineapple/src/Pineapple.java
+++ Pineapple/src/Pineapple.java
@@ -3,6 +3,7 @@ public class Pineapple {
 
 	public static void main(String[] args) {
 		System.out.print("Esta empresa es una mierda");
+		System.out.println("Aclaracion: Empresa de Nacho");
 	}
 
 }
