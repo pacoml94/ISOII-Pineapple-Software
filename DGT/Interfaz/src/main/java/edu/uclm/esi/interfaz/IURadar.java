e4d1bbdf83e178441263bcadf103019e124c8191
Javier <Javier.Zaldivar@alu.uclm.es>
2016-12-06 14:38:57 +0100

    Interfaz de usuario "Radar" creada

---
+++ DGT/Interfaz/src/main/java/edu/uclm/esi/interfaz/IURadar.java
@@ -0,0 +1,69 @@
+package edu.uclm.esi.interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class IURadar {
+
+	private JFrame frmRadar;
+	private final JPanel panel = new JPanel();
+	private final JButton btnArrancarRadar = new JButton("Arrancar radar");
+	private final JButton btnDetenerRadar = new JButton("Detener radar");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IURadar window = new IURadar();
+					window.frmRadar.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public IURadar() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmRadar = new JFrame();
+		frmRadar.setTitle("Radar");
+		frmRadar.setBounds(100, 100, 450, 300);
+		frmRadar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		{
+			frmRadar.getContentPane().add(panel, BorderLayout.CENTER);
+		}
+		panel.setLayout(new GridLayout(2, 1, 0, 0));
+		{
+			panel.add(btnArrancarRadar);
+		}
+		{
+			btnDetenerRadar.addActionListener(new BtnDetenerRadarActionListener());
+			panel.add(btnDetenerRadar);
+		}
+	}
+
+	private class BtnDetenerRadarActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			frmRadar.dispose();
+		}
+	}
+}
