a8025ec8d245a8d11964cb12d94b21b6fd6ea125
pacoml94 <Fco.Murillo@alu.uclm.es>
2016-12-11 12:49:28 +0100

    try catch añadido

--- DGT/src/main/java/edu/uclm/esi/iso2/PINEAPPLE_SOFTWARE/DGT/App.java
+++ DGT/src/main/java/edu/uclm/esi/iso2/PINEAPPLE_SOFTWARE/DGT/App.java
@@ -2,6 +2,7 @@ package edu.uclm.esi.iso2.PINEAPPLE_SOFTWARE.DGT;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -9,14 +10,33 @@ import java.util.Scanner;
  *
  */
 public class App {
-    public static void main( String[] args ) throws SQLException{
-    	Scanner leer= new Scanner(System.in);
-    	MySQLBD bd = new MySQLBD();
-    	bd.conectar();
-    	System.out.println("Pulse 1 para consultar base de datos: ");
-    	int opcion=leer.nextInt();
-    	if(opcion == 1)
-    		consulta(bd);
+    public static void main( String[] args ) {
+		Scanner leer= new Scanner(System.in);
+		boolean repetir = false;
+		
+    	do {
+    		try {
+    			MySQLBD bd = new MySQLBD();
+    			bd.conectar();
+    			System.out.println("Pulse 1 para consultar base de datos: ");
+    			int opcion=leer.nextInt();
+    			if(opcion == 1) {
+    				repetir = true;
+    				consulta(bd);
+    			}
+    		} catch (SQLException e) {
+    			System.err.println("Error al conectar con la base de datos");
+    			leer.nextLine();
+    			repetir = false;
+    		} catch (InputMismatchException e) {
+    			System.err.println("Error al introducir la consulta");
+    			leer.nextLine();
+    			repetir = false;
+    		} catch (Exception e) {
+    			System.err.println("Error general");
+    			leer.nextLine();
+    		}
+		} while (repetir == false);
     }
     
     public static void consulta(MySQLBD bd) throws SQLException{
