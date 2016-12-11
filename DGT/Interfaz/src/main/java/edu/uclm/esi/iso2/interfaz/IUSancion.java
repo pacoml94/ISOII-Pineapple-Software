971e4344b5929cd0b43b61c779b1fe4faa01af87
Javier <Javier.Zaldivar@alu.uclm.es>
2016-12-11 20:30:45 +0100

    Cambio de IU sin completar

--- DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
+++ DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
@@ -20,17 +20,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 public class IUSancion extends JFrame {
 
 	private JPanel contentPane;
-	private final JPanel pnlSancion = new JPanel();
-	private final JLabel lblNmeroDeExpediente = new JLabel();
-	private final JLabel lblDni = new JLabel("DNI de la persona infractora:");
-	private final JTextField txtDNI = new JTextField();
-	private final JButton btnSancionar = new JButton("Sancionar");
 	private int numExp;
 	private MySQLBD bd;
+	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	private final JPanel pnlSanciones = new JPanel();
+	private final JPanel pnlPago = new JPanel();
+	private final JPanel pnlCambiop = new JPanel();
+	private final JScrollPane scrollPane = new JScrollPane();
+	private final JList lstMatriculas = new JList();
+	private final JLabel lblIdDelConductor = new JLabel("ID del conductor: ");
+	private final JLabel lblPuntosDelConductor = new JLabel("Puntos del conductor:");
 	
 	/**
 	 * Launch the application.
@@ -54,38 +60,40 @@ public class IUSancion extends JFrame {
 	public IUSancion() {
 		//Prueba momentanea
 		realizarConsulta();
-		////////////////////
-		txtDNI.setEditable(false);
-		txtDNI.addFocusListener(new TxtDNIFocusListener());
-		txtDNI.setBounds(187, 111, 86, 20);
-		txtDNI.setColumns(10);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 580, 457);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		{
-			contentPane.add(pnlSancion, BorderLayout.CENTER);
+			contentPane.add(tabbedPane, BorderLayout.CENTER);
 		}
-		pnlSancion.setLayout(null);
 		{
-			lblNmeroDeExpediente.setBounds(10, 45, 292, 14);
+			tabbedPane.addTab("Sanciones", null, pnlSanciones, null);
+		}
+		pnlSanciones.setLayout(null);
+		{
+			scrollPane.setBounds(10, 11, 178, 359);
+			pnlSanciones.add(scrollPane);
+		}
+		{
+			scrollPane.setViewportView(lstMatriculas);
+		}
+		{
+			lblIdDelConductor.setBounds(298, 128, 151, 14);
+			pnlSanciones.add(lblIdDelConductor);
+		}
+		{
+			lblPuntosDelConductor.setBounds(298, 167, 132, 14);
+			pnlSanciones.add(lblPuntosDelConductor);
+		}
+		tabbedPane.addTab("Pago de multas", null, pnlPago, null);
+		{
+			tabbedPane.addTab("Cambiar propietario de vehículo", null, pnlCambiop, null);
+		}
+		{
 			numExp = (int) (Math.random()*9999999+1);
-			lblNmeroDeExpediente.setText("Número de expediente de la sanción: "+String.valueOf(numExp));
-			pnlSancion.add(lblNmeroDeExpediente);
-		}
-		{
-			lblDni.setBounds(10, 114, 217, 14);
-			pnlSancion.add(lblDni);
-		}
-		{
-			pnlSancion.add(txtDNI);
-		}
-		{
-			btnSancionar.addActionListener(new BtnSancionarActionListener());
-			btnSancionar.setBounds(159, 182, 114, 23);
-			pnlSancion.add(btnSancionar);
 		}
 		
 	}
@@ -117,24 +125,4 @@ public class IUSancion extends JFrame {
     	}
     	rs.close();
     }
-
-	private class TxtDNIFocusListener extends FocusAdapter {
-		@Override
-		public void focusGained(FocusEvent e) {
-			e.getComponent().setBackground(new Color(250, 250, 210));
-		}
-		@Override
-		public void focusLost(FocusEvent e) {
-			e.getComponent().setBackground(new Color(250, 250, 250));
-		}
-	}
-	private class BtnSancionarActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			String DNI = txtDNI.getText();
-			System.out.printf("Abriendo expediente %d a %s", numExp, DNI);
-			IUPagarSancion pagarSancion = new IUPagarSancion(numExp, DNI);
-			pagarSancion.setVisible(true);
-			dispose();
-		}
-	}
 }
