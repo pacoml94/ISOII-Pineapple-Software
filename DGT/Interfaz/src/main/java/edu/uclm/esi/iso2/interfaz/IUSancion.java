448243a936a77ae8b4bfb23190291eb0c48a868f
Francisco <Fco.Murillo@alu.uclm.es>
2016-12-12 11:08:01 +0100

    Lectura correcta de BBDD

--- DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
+++ DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
@@ -24,6 +24,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTabbedPane;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
 
 public class IUSancion extends JFrame {
 
@@ -35,7 +38,7 @@ public class IUSancion extends JFrame {
 	private final JPanel pnlPago = new JPanel();
 	private final JPanel pnlCambiop = new JPanel();
 	private final JScrollPane scrollPane = new JScrollPane();
-	private final JList<String> lstMatriculas = new JList<String>();
+	private final JList<String> lstConductores = new JList<String>();
 	private final JLabel lblIdDelConductor = new JLabel("ID del conductor: ");
 	private final JLabel lblPuntosDelConductor = new JLabel("Puntos del conductor:");
 	private DefaultListModel<String> modeloLista = new DefaultListModel<>();
@@ -79,14 +82,16 @@ public class IUSancion extends JFrame {
 			pnlSanciones.add(scrollPane);
 		}
 		{
-			scrollPane.setViewportView(lstMatriculas);
+			lstConductores.addListSelectionListener(new LstMatriculasListSelectionListener());
+			lstConductores.setBorder(new TitledBorder(null, "ID conductores", TitledBorder.LEADING, TitledBorder.TOP, null, Color.RED));
+			scrollPane.setViewportView(lstConductores);
 		}
 		{
 			lblIdDelConductor.setBounds(298, 128, 151, 14);
 			pnlSanciones.add(lblIdDelConductor);
 		}
 		{
-			lblPuntosDelConductor.setBounds(298, 167, 132, 14);
+			lblPuntosDelConductor.setBounds(298, 167, 241, 14);
 			pnlSanciones.add(lblPuntosDelConductor);
 		}
 		tabbedPane.addTab("Pago de multas", null, pnlPago, null);
@@ -115,13 +120,31 @@ public class IUSancion extends JFrame {
 	
 	private void leerBD(MySQLBD bd) throws SQLException{
     	ResultSet rs;
-    	String sql = "SELECT * FROM hibernate_sequences";
+    	String sql = "SELECT * FROM driver";
     	rs = bd.consulta(sql);
     	
     	while(rs.next()){
-    		modeloLista.addElement(rs.getString("sequence_name"));
+    		modeloLista.addElement(rs.getString("id"));
     	}
-    	lstMatriculas.setModel(modeloLista);
+    	lstConductores.setModel(modeloLista);
     	rs.close();
     }
+	private class LstMatriculasListSelectionListener implements ListSelectionListener {
+		public void valueChanged(ListSelectionEvent arg0) {
+			ResultSet rs;
+			String sql = "SELECT * FROM driver WHERE id="+lstConductores.getSelectedValue();
+			rs = bd.consulta(sql);
+			
+			try {
+				if (rs.next()) {
+					lblIdDelConductor.setText("ID del conductor: "+rs.getInt("id"));
+					lblPuntosDelConductor.setText("Puntos del conductor: "+rs.getInt("points"));
+				}
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
