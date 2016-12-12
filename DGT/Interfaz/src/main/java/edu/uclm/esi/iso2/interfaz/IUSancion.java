9c1d963a95d42ce8263626264a5dd8b0553044cf
Francisco <Fco.Murillo@alu.uclm.es>
2016-12-12 12:35:43 +0100

    BBDD peta

--- DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
+++ DGT/Interfaz/src/main/java/edu/uclm/esi/iso2/interfaz/IUSancion.java
@@ -8,7 +8,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import edu.uclm.esi.iso2.multas.dao.DriverDao;
 import edu.uclm.esi.iso2.persistencia.MySQLBD;
+import edu.uclm.esi.iso2.multas.domain.*;
 
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
@@ -26,6 +28,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ListSelectionListener;
+
+import com.mysql.cj.jdbc.Driver;
+
 import javax.swing.event.ListSelectionEvent;
 
 public class IUSancion extends JFrame {
@@ -42,6 +47,14 @@ public class IUSancion extends JFrame {
 	private final JLabel lblIdDelConductor = new JLabel("ID del conductor: ");
 	private final JLabel lblPuntosDelConductor = new JLabel("Puntos del conductor:");
 	private DefaultListModel<String> modeloLista = new DefaultListModel<>();
+	private final JLabel lblIdDelConductor_1 = new JLabel("ID del conductor:");
+	private final JLabel lblFechaDeLa = new JLabel("Fecha de la sanción:");
+	private final JLabel lblZonaDeLa = new JLabel("Zona de la multa: ");
+	private final JLabel lblVelocidadDelVehculo = new JLabel("Límite de velocidad:");
+	private final JLabel lblVelocidadDelVehculo_1 = new JLabel("Velocidad del vehículo:");
+	private final JLabel lblIdDeLa = new JLabel("ID de la sanción: ");
+	private DriverDao driverDao;
+	private final JLabel lblValorID = new JLabel("");
 	/**
 	 * Launch the application.
 	 */
@@ -87,14 +100,43 @@ public class IUSancion extends JFrame {
 			scrollPane.setViewportView(lstConductores);
 		}
 		{
-			lblIdDelConductor.setBounds(298, 128, 151, 14);
+			lblIdDelConductor.setBounds(298, 128, 93, 14);
 			pnlSanciones.add(lblIdDelConductor);
 		}
 		{
 			lblPuntosDelConductor.setBounds(298, 167, 241, 14);
 			pnlSanciones.add(lblPuntosDelConductor);
 		}
+		{
+			lblValorID.setBounds(398, 128, 46, 14);
+			pnlSanciones.add(lblValorID);
+		}
 		tabbedPane.addTab("Pago de multas", null, pnlPago, null);
+		pnlPago.setLayout(null);
+		{
+			lblIdDelConductor_1.setBounds(34, 68, 110, 14);
+			pnlPago.add(lblIdDelConductor_1);
+		}
+		{
+			lblFechaDeLa.setBounds(34, 117, 135, 14);
+			pnlPago.add(lblFechaDeLa);
+		}
+		{
+			lblZonaDeLa.setBounds(34, 158, 160, 14);
+			pnlPago.add(lblZonaDeLa);
+		}
+		{
+			lblVelocidadDelVehculo.setBounds(368, 86, 135, 14);
+			pnlPago.add(lblVelocidadDelVehculo);
+		}
+		{
+			lblVelocidadDelVehculo_1.setBounds(368, 117, 135, 14);
+			pnlPago.add(lblVelocidadDelVehculo_1);
+		}
+		{
+			lblIdDeLa.setBounds(368, 158, 110, 14);
+			pnlPago.add(lblIdDeLa);
+		}
 		{
 			tabbedPane.addTab("Cambiar propietario de vehículo", null, pnlCambiop, null);
 		}
@@ -137,14 +179,21 @@ public class IUSancion extends JFrame {
 			
 			try {
 				if (rs.next()) {
-					lblIdDelConductor.setText("ID del conductor: "+rs.getInt("id"));
+					lblValorID.setText(String.valueOf(rs.getInt("id")));
 					lblPuntosDelConductor.setText("Puntos del conductor: "+rs.getInt("points"));
+					driverDao = new DriverDao();
 				}
 				rs.close();
+				sancion(driverDao, Integer.parseInt(lblValorID.getText()));
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+		
+		private void sancion(DriverDao d, int id) {
+			edu.uclm.esi.iso2.multas.domain.Driver driver = d.findById(getClass(), id);
+			lblIdDelConductor_1.setText(String.valueOf(driver.getId()));
 		}
 	}
 }
